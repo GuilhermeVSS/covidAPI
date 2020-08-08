ee1a349636251a8c5bf7ae00d17941d318304055
Guilherme Ventura <gvssilva@dcomp.ufs.br>
2020-08-08 15:10:59 -0300

    feat(heroku config): add the files needed to upload the API to Heroku

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 const express = require('express');
-
 const app = express();
-
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 3333;
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 require('./routes')(app);
 
-app.listen(3333);
+app.listen(port);
