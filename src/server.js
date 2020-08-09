2ee57d05e9357a4d66d1a97ea7bb40b6b36e8b90
Guilherme Ventura <gvssilva@dcomp.ufs.br>
2020-08-09 13:05:19 -0300

    feat(cors): add cors dependencie

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3333;
+const cors = require('cors')
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(cors());
 //main route
-app.get('/', (request, response) => {
-    response.json({
+app.get('/', (req, res) => {
+    res.json({
         Info: 'covidAPI',
         github: 'https://github.com/GuilhermeVSS',
         contact: 'guilherme_ventura.ss@outlook.com',
